ce11b0c6fc77e87ca11ad42b0561280dbe69cb18
Golodhrim <holidayninjastaff@gmail.com>
2020-09-12 09:58:35 -0400

    banner fixes

--- src/components/BadIdea.js
+++ src/components/BadIdea.js
@@ -7,7 +7,7 @@ export default class BadIdea extends React.Component {
         return (
             <div>
                 <div className="container">
-                    <h1 className="container__idea">BAD <span className="text-transform">Business Idea</span><span className="text-transform__idea"> by BADLY</span></h1>
+                    <h2 className="container__idea">BAD <span className="text-transform">Business Idea</span><span className="text-transform__idea"> by BADLY</span></h2>
                     <h5>{this.props.title}</h5>
                     <p className="text-transform-margin__idea">{this.props.idea}</p>
                     <button 
