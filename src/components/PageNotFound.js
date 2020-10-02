64532cec8fa6e3cfb76ae73c9588bbe9fa385ded
Golodhrim <holidayninjastaff@gmail.com>
2020-10-02 11:56:32 -0400

    404 much empty

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,12 +3,15 @@ import React from 'react'
 const PageNotFound = () => (
     <div className="container__flex-footer">
         <div className="container">
+        <div>
+        <p className="container__team--pic--text">Wow - much empty</p>
+        </div>
             <div className="doge">
                 <img className="img_doge" src="https://i.imgur.com/iAGprY4.png"></img>
             </div>
-    
+        
         <h2>
-            Oh <span className="text-transform">Henro Fren</span>
+            404 <span className="text-transform">Badly Fren</span>
         </h2>
         <p className="container__ptext">
             You've discovered Doge.  Doge doin a surprise.  Doge is happy to see you.
